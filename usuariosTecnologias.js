85e4082b127674f871a37601c3fd744215b0ca12
WebertiBarbosa <30930934+WebertiBarbosa@users.noreply.github.com>
2020-06-16 22:15:40 -0300

    Create usuariosTecnologias.js

---
+++ usuariosTecnologias.js
@@ -0,0 +1,36 @@
+// const usuarios = [
+//     {
+//         nome: 'Carlos',
+//         tecnologias: ['HTML', 'CSS']
+//     },
+//     {
+//         nome: 'Jasmine',
+//         tecnologias: ['JavaScript', 'CSS']
+//     },
+//     {
+//         nome: 'Tuane',
+//         tecnologias: ['HTML', 'Node.js']
+//     }
+// ]
+
+// function imprimeUsuario(usuarios) {
+//     for (let i = 0; i < usuarios.length; i++) {
+//         console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias.join(', ')}`)
+//     }
+//     return usuarios
+// }
+
+// const programador = imprimeUsuario(usuarios)
+
+
+// opção mais simples
+
+const usuarios = [
+    { nome: 'Carlos', tecnologias: ['HTML', 'CSS']},
+    { nome: 'Jasmine', tecnologias: ['JavaScript', 'CSS']},
+    { nome: 'Tuane', tecnologias: ['HTML', 'Node.js']}
+]
+
+for (let usuario of usuarios) {
+    console.log(`${usuario.nome} trabalha com ${usuario.tecnologias.join(', ')}`)
+}
